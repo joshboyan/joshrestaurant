a5fa2c5e52536bde5e224ee06ecf0b3708dda966
josh.boyan <josh.boyan@bestbuy.com>
2018-02-13 20:13:13 -0600

    Flesh out gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,24 @@
 "use strict";
-
-// packages
+//imports
 const gulp = require('gulp');
-const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create(); // create a browser sync instance.
 const htmlbeautify = require('gulp-html-beautify');
+const less = require('gulp-less');
+const path = require('path');
 
 //paths
 const htmlSources = './*.html';
 const cssSources = './css/*.less'
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            //Change address below to load different page with browserSync
-            index: "index.html"
-        }
-    });
+gulp.task('less', function () {
+  return gulp.src(cssSources)
+    .pipe(less())
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('html', function() {
+gulp.task('htmlbeautify', function() {
     gulp.src('./*.html')
       .pipe(htmlbeautify({indentSize: 2}))
       .pipe(gulp.dest('./'))
   });
 
-gulp.task('css', function() {
-    gulp.src(cssSources)
-        .pipe(less())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest(function(file){
-            return file.base
-        }))
-})
-
-gulp.task('watch', function() {
-    gulp.watch(htmlSources, ['html']).on('change', browserSync.reload);
-    gulp.watch(cssSources, ['css']).on('change', browserSync.reload);
-});
-
-gulp.task('default', ['browser-sync', 'watch'])
+gulp.task('default', ['htmlbeautify', 'less']);
