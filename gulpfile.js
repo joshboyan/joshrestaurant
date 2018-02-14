37b14e850d8aecf9972bbac7a290c56c6b3b574b
josh.boyan <josh.boyan@bestbuy.com>
2018-02-13 21:13:09 -0600

    Watch tasks and hot-reloading

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,19 @@ const gulp = require('gulp');
 const htmlbeautify = require('gulp-html-beautify');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
+const bs = require('browser-sync').create(); // create a browser sync instance.
 
 //paths
 const htmlSources = './*.html';
 const cssSources = './css/*.less'
+
+gulp.task('browser-sync', function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('less', function () {
   return gulp.src(cssSources)
@@ -25,4 +34,9 @@ gulp.task('htmlbeautify', function() {
       .pipe(gulp.dest('./'))
   });
 
-gulp.task('default', ['htmlbeautify', 'less']);
+gulp.task('watch', function() {
+    gulp.watch(cssSources, ['less']).on('change', bs.reload);
+    gulp.watch(htmlSources, ['htmlbeautify']).on('change', bs.reload);
+})
+
+gulp.task('default', ['browser-sync', 'watch']);
